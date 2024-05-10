fbef869227323ec8292dabc82dfecd8057ff7d01
Ivo Richert <ivo.richert@gmail.com>
2024-05-10 13:13:30 +0200

    Finished function for offline calculation of S

--- FilterCheck.py
+++ FilterCheck.py
@@ -62,14 +62,20 @@ dictd = return_dict(d, order=4)
 dictk = return_dict(k, order=4)
 
 
-def S_func(dict1, dict2, trans):
-    raw_collections = np.array([np.sort(dictk[i][dictk[i] != 0]).tolist() for i in range(n_dim(k, order=4))], dtype=object)
-    locations = [np.where(np.in1d(dictk[i], raw_collections[i]))[0][np.argsort(dictk[i][dictk[i] != 0])] for i in range(n_dim(k, order=4))]
-    collections = np.array([x for i, x in enumerate(raw_collections.tolist()) if x not in raw_collections.tolist()[:i]], dtype=object)
-    coll_locator = np.array([collections.tolist().index(x) for x in raw_collections])
+def S_func(trans, order):
+    dim1, dim2 = trans.shape
+    dict1, dict2 = return_dict(dim1, order), return_dict(dim2, order)
+    raw_collections1 = np.array([np.sort(dict1[i][dict1[i] != 0]).tolist() for i in range(n_dim(dim1, order=4))], dtype=object)
+    raw_collections2 = np.array([np.sort(dict2[i][dict2[i] != 0]).tolist() for i in range(n_dim(dim2, order=4))], dtype=object)
+    locations1 = [np.where(np.in1d(dict1[i], raw_collections1[i]))[0][np.argsort(dict1[i][dict1[i] != 0])] for i in range(n_dim(dim1, order=4))]
+    locations2 = [np.where(np.in1d(dict2[i], raw_collections2[i]))[0][np.argsort(dict2[i][dict2[i] != 0])] for i in range(n_dim(dim2, order=4))]
+    collections1 = np.array([x for i, x in enumerate(raw_collections1.tolist()) if x not in raw_collections1.tolist()[:i]], dtype=object)
+    collections2 = np.array([x for i, x in enumerate(raw_collections2.tolist()) if x not in raw_collections2.tolist()[:i]], dtype=object)
+    coll_locator1 = np.array([collections1.tolist().index(x) for x in raw_collections1])
+    coll_locator2 = np.array([collections2.tolist().index(x) for x in raw_collections2])
 
-    raw_combinations = np.array(list(product(collections, collections)), dtype=object)
-    coll_combinations = np.array(sum([list(product(list(compress(collections, [np.sum(coll) == i for coll in collections])), list(compress(collections, [np.sum(coll) == i for coll in collections])))) for i in range(1, 5)], []), dtype=object)
+    raw_combinations = np.array(list(product(collections1, collections2)), dtype=object)
+    coll_combinations = np.array(sum([list(product(list(compress(collections1, [np.sum(coll) == i for coll in collections1])), list(compress(collections2, [np.sum(coll) == i for coll in collections2])))) for i in range(1, 5)], []), dtype=object)
     raw_comb_locator = []
     for comb in raw_combinations:
         try:
@@ -93,24 +99,25 @@ def S_func(dict1, dict2, trans):
 
     solutions = np.array(solutions, dtype=object)
 
-    I, J = np.meshgrid(np.arange(n_dim(k, order=4)), np.arange(n_dim(k, order=4)))
-    S_tilde = np.zeros((n_dim(k, order=4), n_dim(k, order=4)))
-    for i in trange(n_dim(k, order=4)):
-        index_in_raw_comb = np.where((raw_combinations == np.expand_dims(np.array([collections[coll_locator[I[:, i]]], collections[coll_locator[J[:, i]]]]).T, -2)).all(-1))[-1]
+    I, J = np.meshgrid(np.arange(n_dim(dim1, order=4)), np.arange(n_dim(dim2, order=4)))
+    S = np.zeros((n_dim(dim1, order=4), n_dim(dim2, order=4)))
+    for i in trange(n_dim(dim1, order=4)):
+        index_in_raw_comb = np.where((raw_combinations == np.expand_dims(np.array([collections1[coll_locator1[I[:, i]]], collections2[coll_locator2[J[:, i]]]]).T, -2)).all(-1))[-1]
         sol_locator = raw_comb_locator[index_in_raw_comb]
 
         sol = solutions[sol_locator]
-        for j in range(n_dim(k, order=4)):
+        for j in range(n_dim(dim2, order=4)):
             if sol_locator[j] == -1:
-                S_tilde[i, j] = 0
+                S[i, j] = 0
                 continue
-            large_solution = np.zeros((sol[j].shape[0], k, k))
-            large_solution[np.ix_(np.arange(sol[j].shape[0]), locations[i], locations[j])] = sol[j]
-            S_tilde[i, j] = np.prod(multinom(large_solution) * mult_pow(Y, large_solution), axis=1).sum()
-    return S_tilde
+            large_solution = np.zeros((sol[j].shape[0], dim1, dim2))
+            large_solution[np.ix_(np.arange(sol[j].shape[0]), locations1[i], locations2[j])] = sol[j]
+            S[i, j] = np.prod(multinom(large_solution) * mult_pow(trans, large_solution), axis=1).sum()
+    return S
 
 
-S_mat = S_func(dictk, Y)
+S_mat = S_func(Y, order=4)
+S_mat_d = S_func(C, order=4)
 
 
 def S_tilde_func(mu_tilde, eta, Y):
@@ -128,6 +135,23 @@ def S_tilde_func(mu_tilde, eta, Y):
     sol_vec[np.ix_(np.arange(sol.shape[0]), eta > 0, mu_tilde > 0)] = sol
 
     return np.prod(multinom(sol_vec) * mult_pow(Y, sol_vec), axis=1).sum()
+
+
+def S_d_func(mu, eta, C):
+    dim_mat = (mu > 0).sum(), (eta > 0).sum()
+    A = np.zeros((np.sum(dim_mat), np.prod(dim_mat)))
+    A[np.repeat(np.arange(dim_mat[0]), dim_mat[1]), np.arange(A.shape[1])] = 1
+    A[np.tile(np.arange(dim_mat[1]), dim_mat[0]) + dim_mat[0], np.arange(A.shape[1])] = 1
+    b = np.hstack((mu[mu > 0], eta[eta > 0]))
+    R = np.eye(A.shape[1])
+    sol = solve_int(A, b, R)
+    sol = sol.reshape((sol.shape[0], dim_mat[0], dim_mat[1]))
+    sol = np.transpose(sol, axes=(0, 2, 1))
+
+    sol_vec = np.zeros((sol.shape[0], eta.shape[0], mu.shape[0]))
+    sol_vec[np.ix_(np.arange(sol.shape[0]), eta > 0, mu > 0)] = sol
+
+    return np.prod(multinom(sol_vec) * mult_pow(C, sol_vec), axis=1).sum()
 
 
 # Check equations
@@ -215,6 +239,9 @@ for mu_tild in tqdm(mu_tilde):
     temp.append(mult_pow(Zs, mu_tild) * c_lambt_mut)
 result6 = mult_pow(Xt, lamb) * np.sum(temp)
 
+
+S_mat = S_func(Y, order=4)
+S_mat_d = S_func(C, order=4)
 mu_tilde = dictk[mask(lamb_tilde, dictk, typ='leq_abs')]
 S_tilde_mat = S_func
 temp = []
